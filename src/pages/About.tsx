40b51ef6f5cf0e690c137c9b3910ca04f382a2af
Owen Runge <owen@owenrunge.com>
2023-10-26 12:40:36 -0400

    MAJOR ISSUES WITH THIS VERSION

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -5,7 +5,8 @@ const About = () => {
     return (
         <div>
             <h1>About Us</h1>
-            <img src={Barnett_Joe_Hayes_Drake1} alt='DRAKE IS RUNNING DRAKE IS RUNNING' height='333.25' width='500'/>
+            {/* <img src={Barnett_Joe_Hayes_Drake1} alt='DRAKE IS RUNNING DRAKE IS RUNNING' height='333.25' width='500'/> */}
+            <p>We are frauds.</p>
         </div>
     );
 };
--- src/pages/Survey.tsx
+++ src/pages/Survey.tsx
@@ -8,12 +8,53 @@ import { json } from "./json";
 //import { saveAs } from 'file-saver';
 //import * as jsonfile from 'jsonfile';
 //import * as fs from 'fs';
+import { spawnSync } from 'child_process';
+import { readFile } from 'jsonfile';
+import { useCallback } from "react";
 
 function SurveyComponent() {
     const survey = new Model(json);
     survey.applyTheme(SurveyTheme.SharpDark);
-    survey.onComplete.add((sender, options) => {
-        console.log(JSON.stringify(sender.data, null, 3));
+    const surveyComplete = useCallback((sender: any) => {
+        handleSurveyResults(
+            sender.data
+        )
+    }, []);
+
+    survey.onComplete.add(surveyComplete);
+    // survey.onComplete.add(async (sender, options) => {
+    //     console.log(JSON.stringify(sender.data, null, 3));
+    //     // const path = './surveyResults.json';
+    //     // fs.writeFile(path, JSON.stringify(sender.data, null, 3), (error: any) => {
+    //     //     if (error) {
+    //     //         console.log('An error has occured', error);
+    //     //         return;
+    //     //     }
+    //     //     console.log('data successfully written to file.');
+    //     // });
+    //     const pythonProcess = await spawnSync('python3', [
+    //         '../scripts/PYTHONFILENAME.py',
+    //         'FUNCTIONNAME',
+    //         JSON.stringify(sender.data),
+    //         '../scripts/modelResults.json'
+    //     ]);
+    //     const result = pythonProcess.stdout?.toString()?.trim();
+    //     const error = pythonProcess.stderr?.toString()?.trim();
+
+    //     const status = result === 'OK';
+    //     if (status) {
+    //         const buffer = await readFile('../scripts/modelResults.json');
+    //         const resultParsed = JSON.parse(buffer?.toString());
+    //         console.log(resultParsed);
+    //     } else {
+    //         console.log(error);
+    //     }
+    // });
+    return (<Survey model={survey} />);
+}
+
+async function handleSurveyResults(json: any) {
+    console.log(JSON.stringify(json, null, 3));
         // const path = './surveyResults.json';
         // fs.writeFile(path, JSON.stringify(sender.data, null, 3), (error: any) => {
         //     if (error) {
@@ -22,8 +63,23 @@ function SurveyComponent() {
         //     }
         //     console.log('data successfully written to file.');
         // });
-    });
-    return (<Survey model={survey} />);
+        const pythonProcess = await spawnSync('python3', [
+            '../scripts/test.py',
+            'model_output',
+            JSON.stringify(json),
+            '../scripts/modelResults.json'
+        ]);
+        const result = pythonProcess.stdout?.toString()?.trim();
+        const error = pythonProcess.stderr?.toString()?.trim();
+
+        const status = result === 'OK';
+        if (status) {
+            const buffer = await readFile('../scripts/modelResults.json');
+            const resultParsed = JSON.parse(buffer?.toString());
+            console.log(resultParsed);
+        } else {
+            console.log(error);
+        }
 }
 
 export default SurveyComponent;
