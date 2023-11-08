33d121e5c2d8ab7be3214d569caed1dc2e0dd2d8
Owen Runge <owen@owenrunge.com>
2023-11-07 21:31:42 -0500

    styled homepage and modified about and contact page styling

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Contact = () => {
     return (
-        <div className="bg-dark text-white h-screen">
-            <h1 className="text-inherit font-bold p-3">Contact Us!</h1>
-            <p className="text-inherit pl-3 py-0">Please send any and all complaints to the following email addresses:</p>
-            <ul className="pl-4 space-y-0.5">
-                <li className="text-inherit"/> drhayes24@wabash.edu
-                <li className="text-inherit"/> orrunge24@wabash.edu
-                <li className="text-inherit"/> khoque24@wabash.edu
+        <div className="bg-dark text-white text-base font-jetbrainsmono min-h-screen max-h-full">
+            <h1 className="text-inherit text-5xl font-bold py-10 px-5">Contact Us!</h1>
+            <p className="text-inherit pl-5 py-0">Please send any and all complaints to the following email addresses:</p>
+            <ul className="text-inherit pl-5 space-y-0.5 list-disc list-inside space-y-1">
+                <li>drhayes24@wabash.edu</li>
+                <li>orrunge24@wabash.edu</li>
+                <li>khoque24@wabash.edu</li>
             </ul>
         </div>
     );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -17,11 +17,32 @@ const ColorButton = styled(Button)<ButtonProps>(({ theme }) => ({
 
 const Home = () => {
     return (
-        <div className='bg-dark h-screen text-white'>
-            <h1 className='text-inherit font-bold p-3'>Home Page</h1>
-            <p className='text-inherit pl-3'>Welcome to our cool website!!! Take the survey below to get your vacation recommendations!!!</p>
-            <div className='pl-3'>
-                <ColorButton variant='contained' size='large' href='/survey/' sx={{margin:'5px',}}>To Survey</ColorButton>
+        <div className='bg-dark h-full text-white'>
+            <h1 className='text-inherit text-5xl font-jetbrainsmono font-bold px-5 py-10'>Welcome to Vacation Recommender!</h1>
+            <div className='px-5'>
+                <p className='text-inherit text-xl font-jetbrainsmono'>Vacation Recommender is an all-in-one tool to find the vacation destination of your dreams.</p>
+                <p className='text-inherit text-base font-jetbrainsmono py-2'>Simply take the survey on the following criteria to find the cities<sup>*</sup> that best fit your wants</p>
+                <ul className='text-inherit text-base font-jetbrainsmono list-disc list-outside hover:list-inside px-8 space-y-1'>
+                    <li>climate</li>
+                    <li>transportation</li>
+                    <li>dining</li>
+                    <li>attractions</li>
+                    <li>lodging</li>
+                    <li>safety</li>
+                    <li>health</li>
+                </ul>
+                <p className='text-inherit text-base font-jetbrainsmono py-2'>Upon submission, you will be greeted with your top 5 cities specifically catered to you!</p>
+                <p className='text-neutral-600 text-sm font-jetbrainsmono'><sup>*</sup>Cities limited to those within the U.S. and Puerto Rico</p>
+            </div>
+            <div className='mx-3 mt-5 bg-custom-foreground p-4 border-2 rounded border-custom-border'>
+                <div className='flex flex-col justify-center items-center'>
+                    <h1 className='text-inherit text-3xl font-jetbrainsmono'>Click the button below to take the survey</h1>
+                    <div className='pt-5'>
+                        <ColorButton variant='contained' size='large' href='/survey/' sx={{fontSize:20}}>To Survey</ColorButton>
+                    </div>
+                </div>
+            </div>
+            <div className='bg-dark h-12'>
             </div>
         </div>
     );
